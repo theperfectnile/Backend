d11b5d26c1d0d33c8d495b16662814222aded3ff
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-17 13:48:08 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/authController");
+const { register, login, forgotPassword } = require("../controllers/authController");
 
+// Auth routes
 router.post("/register", register);
 router.post("/login", login);
 
+// Forgot password route
+router.post("/forgot-password", forgotPassword);
+
 module.exports = router;
+
