27193b8e3eeb00b64a54aa3e1f576de7861c33d9
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-04-18 18:58:01 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
