02f19d2bc298c7dce0b634c05e715f768b26af6e
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-05-30 22:06:22 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,3 +76,9 @@ exports.getUser = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+module.exports = {
+  register,
+  login,
+  forgotPassword,
+  getUser
+};
