0aaada878bf84a825e31e3a825579008fbbb066e
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-05-30 22:05:41 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,3 +64,15 @@ exports.forgotPassword = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+// ⭐ GET LOGGED-IN USER
+const User = require("../models/User");
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error("GET USER ERROR:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
