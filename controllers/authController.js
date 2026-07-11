8b5ba20d79f663a9ab0dbf95e75b1d50e89b0007
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-07-11 09:38:08 -0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,9 +14,18 @@ exports.register = async (req, res) => {
 
     const hashed = await bcrypt.hash(password, 10);
 
-    await User.create({ email, password: hashed });
+   const newUser = await User.create({ email, password: hashed });
 
-    res.json({ message: "Registered successfully" });
+const token = jwt.sign(
+  { id: newUser._id },
+  process.env.JWT_SECRET,
+  { expiresIn: "7d" }
+);
+
+res.json({
+  message: "Registered successfully",
+  token
+});
   } catch (err) {
     console.error("REGISTER ERROR:", err);
     res.status(500).json({ message: "Server error" });
@@ -36,7 +45,11 @@ exports.login = async (req, res) => {
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+  { id: user._id },
+  process.env.JWT_SECRET,
+  { expiresIn: "7d" }
+);
 
     res.json({ token });
   } catch (err) {
