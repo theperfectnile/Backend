5711796f8f0e2c7c1e284fa321f410aa6a6314f9
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-08-22 08:40:06 -0700

    Rebuild repo with correct backend structure

--- models/Survey.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SurveySchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  answers: { type: Array, default: [] },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Survey", SurveySchema);
--- models/XPModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const XPSchema = new mongoose.Schema({
-  userId: String,
-  xp: Number,
-  log: Array
-});
-
-module.exports = mongoose.model("XP", XPSchema);
