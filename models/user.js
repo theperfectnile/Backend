45b820b65680072431308b1bb33134e2fb33bdcd
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-08-22 09:58:10 -0700

    Modify user schema to add subscriptionActive field
    Updated user schema to include subscriptionActive field and removed required constraints.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  subscriptionActive: Boolean
+});
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("User", UserSchema);
