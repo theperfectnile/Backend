c9bcbb8f49eb3c63ee84952380b7897c9ff294e7
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-08-22 09:40:09 -0700

    Delete Backend directory

--- Backend/xp.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const XpSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  xp: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model("Xp", XpSchema);
