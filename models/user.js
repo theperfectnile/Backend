a357efb9c7208207d26b63d983f2c64b6fbf1499
theperfectnile <58378808+theperfectnile@users.noreply.github.com>
2026-08-22 10:08:06 -0700

    Delete models directory

--- models/user.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  subscriptionActive: Boolean
-});
-
-export default mongoose.model("User", UserSchema);
